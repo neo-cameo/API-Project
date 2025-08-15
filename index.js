3ff5c4283cbd99a8c8a745cec112b4c9102bf329
Maceo <ssjmaceo@gmail.com>
2025-08-14 23:51:40 -0400

    fifth commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-
+// const movieTitle = document.getElementById('movieTitle')
+// movieTitle.addEventListener('keyup', e => {
+//     let currentValue = e.target.value.toLowerCase()
+//     console.log(currentValue)
+// })
 
 async function movieSearch() {
-    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=50a9856f&s=${input}`)
-    const data = await response.json()
-    console.log(data)
+  const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=50a9856f&s=soldier`);
+  const moviesData = await movies.json();
+    movieListEl = document.querySelector(".movie")
+    console.log(moviesData.Search)
 
+  movieListEl.innerHTML =
+    moviesData.Search.map(
+      (movie) => `<figure class="poster">
+                    <img src="${movie.Poster}" alt="">
+                </figure>
+                <div class="title">${movie.Title}</div>
+                <div class="year">${movie.Year}</div>`
+    )
+    .join("")
 }
 
-movieSearch()
+movieSearch();
