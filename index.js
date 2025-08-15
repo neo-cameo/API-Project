f6449f2e1343c7fd309dc7012525077853bd7dad
Maceo <ssjmaceo@gmail.com>
2025-08-15 00:03:19 -0400

    sixth commit

--- index.js
+++ index.js
@@ -7,16 +7,19 @@
 async function movieSearch() {
   const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=50a9856f&s=soldier`);
   const moviesData = await movies.json();
-    movieListEl = document.querySelector(".movie")
+    movieListEl = document.querySelector(".movie__container")
     console.log(moviesData.Search)
 
   movieListEl.innerHTML =
     moviesData.Search.map(
-      (movie) => `<figure class="poster">
+      (movie) => `
+            <div class="movie">
+                <figure class="poster">
                     <img src="${movie.Poster}" alt="">
                 </figure>
                 <div class="title">${movie.Title}</div>
-                <div class="year">${movie.Year}</div>`
+                <div class="year">${movie.Year}</div>
+                </div>`
     )
     .join("")
 }
