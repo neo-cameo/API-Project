4bd0e7688a529e272046ff696d6625208d6b46fb
Maceo <ssjmaceo@gmail.com>
2025-08-17 01:13:03 -0400

    eigth commit

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-// function searchQueary() {
-// const movieTitle = document.getElementById('movieTitle');
-// movieTitle.addEventListener('keyup', e => {
-//     let currentValue = e.target.value.toLowerCase()
-//     console.log(currentValue)
-// })
-// }
 
 async function movieSearch() {
     const title = document.getElementById('movieTitle').value
-  const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=50a9856f&s=${title}`);
-  const moviesData = await movies.json();
+    const moviesLoad = document.querySelector('.movies__list')
+
+    moviesLoad.classList += ' movies__loading'
+
+    if (!movies) {
+            movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=50a9856f&s=${title}`);
+     moviesData = await movies.json();
     movieListEl = document.querySelector(".movie__container")
     console.log(moviesData.Search)
 
-  movieListEl.innerHTML =
-    moviesData.Search.map(
-      (movie) => `
-            <div class="movie">
-                <figure class="poster">
-                    <img src="${movie.Poster}" alt="">
-                </figure>
-                <div class="title">${movie.Title}</div>
-                <div class="year">${movie.Year}</div>
-                </div>`
-    )
-    .join("")
+    moviesLoad.classList.remove('movies__loading')
+
+    if (moviesData.Response === "True" && Array.isArray(moviesData.Search)) {
+        movieListEl.innerHTML =
+          moviesData.Search.map(
+            (movie) => `
+                  <div class="movie">
+                      <figure class="poster">
+                          <img src="${movie.Poster}" alt="">
+                      </figure>
+                      <div class="title">${movie.Title}</div>
+                      <div class="year">${movie.Year}</div>
+                      </div>`
+          )
+          .join("")
+          return
+    }
+    else {
+        movieListEl.innerHTML = `<div>Make sure your spelling is correct and a title is in the search bar!</div>`
+    }
+    }
+  
 }
 
-movieSearch();
